5df7fb5b87d0bf3a4ebb1dd398c90b2f81d4e6cb
Dániel Kása <lord-dani@hotmail.com>
2024-06-17 18:47:39 +0200

    Test remove from cart

--- SwagLabsTest/SauceTest.TestRemoveFromCart.cs
+++ SwagLabsTest/SauceTest.TestRemoveFromCart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace SwagLabsTest
 {
     public partial class SauceTests
     {
+        [Test]
+        public void TestRemoveFromCart()
+        {
+            var addToCartButton = myDriver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
+            Assert.That(addToCartButton.Displayed, Is.True);
+            Assert.That(addToCartButton.Enabled, Is.True);
+            Assert.That(addToCartButton.Text, Is.EqualTo("Add to cart"));
+            addToCartButton.Click();
+            var cartCount = myDriver.FindElement(By.ClassName("shopping_cart_badge"));
+            Assert.That(cartCount.Text, Is.EqualTo("1"));
+            Assert.That(addToCartButton.Text, Is.EqualTo("Remove"));
+
+            addToCartButton.Click();
+            Assert.That(cartCount.Text, Is.EqualTo("0"));
+            Assert.That(addToCartButton.Text, Is.EqualTo("Add to cart"));
+            myDriver.FindElement(By.Id("shopping_cart_container")).Click();
+
+            var cartItems = myDriver.FindElement(By.Id("cart_contents_container")).FindElements(By.ClassName("cart_item"));
+
+            Assert.That(cartItems.Count, Is.EqualTo(0));
+        }
     }
 }
